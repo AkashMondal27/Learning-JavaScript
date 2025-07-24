a92d97103e314fb4c2590feff242b5c01fcb7b97
AkashMondal27 <akashmondal102003@gmail.com>
2025-07-24 17:18:53 +0530

    Add initial HTML and JavaScript files for conditional statement examples

---
+++ ConditionalStatement/question.js
@@ -0,0 +1,9 @@
+/* get a user input using promt ,cheack if the number is multiple by 5 or not */
+
+let num=prompt("Enter a number :");
+if( num %5 ==0){
+    console.log("The numbeer is multiple by 5 .");
+}
+else {
+    console.log("The number is not mltiple by 5 ");
+}
